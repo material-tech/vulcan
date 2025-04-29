f84e2ed96525a594f33fa214d2d518c434b4c34a
enpitsulin <enpitsulin@gmail.com>
2025-04-29 14:21:27 +0800

    fix: improve expected extend

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,20 +1,37 @@
 import type { Dnum } from 'dnum'
-import { eq, format } from 'dnum'
+import { toNumber } from 'dnum'
 import { expect } from 'vitest'
 
 expect.extend({
-  toMatchNumberArray(received: Dnum[], expected: number[]) {
-    const { isNot } = this
+  toMatchNumberArray(received: Dnum[], expected: number[], options?: { digits?: number }) {
+    const { isNot, utils } = this
+    const receivedNumbers = received.map(v => toNumber(v, { digits: options?.digits }))
+    const pass = this.equals(receivedNumbers, expected)
     return {
-      pass: received.length === expected.length && received.every((value, index) => eq(value, expected[index])),
-      message: () => `${received.map(format)} is${isNot ? ' not' : ''} match ${expected}`,
+      pass,
+      message: () => {
+        return `expected ${JSON.stringify(receivedNumbers)} to ${isNot ? 'not' : ''} match ${JSON.stringify(expected)}\n${utils.printDiffOrStringify(receivedNumbers, expected)}`
+      },
     }
   },
-  toMatchNumber(received: Dnum, expected: number) {
-    const { isNot } = this
+  toMatchNumber(received: Dnum, expected: number, options?: { digits?: number }) {
+    const { isNot, utils } = this
+    const receivedNumber = toNumber(received, { digits: options?.digits })
     return {
-      pass: eq(received, expected),
-      message: () => `${received} is${isNot ? ' not' : ''} match ${expected}`,
+      pass: this.equals(receivedNumber, expected),
+      message: () => `expected ${receivedNumber} is${isNot ? ' not' : ''} to match ${expected}\n${utils.printDiffOrStringify(receivedNumber, expected)}`,
     }
   },
 })
+
+interface CustomMatchers<R = unknown> {
+  /** match Dnum[] with number[] */
+  toMatchNumberArray: (expected: number[], options?: { digits?: number }) => R
+  /** match Dnum with number */
+  toMatchNumber: (expected: number, options?: { digits?: number }) => R
+}
+
+declare module 'vitest' {
+  interface Assertion<T = any> extends CustomMatchers<T> {}
+  interface AsymmetricMatchersContaining extends CustomMatchers {}
+}
